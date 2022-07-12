ba117c2286192f2e65534407977fc4278c8e241f
Zac Burrage <zacburrage@c02gr5grq05n.home>
2022-07-12 11:45:27 -0500

    server

--- server.js
+++ server.js
@@ -29,6 +29,19 @@ app.use(
   })
 );
 
-app.use(express.static(join(__dirname, 'dist')));
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(express.static('./dist/zac0'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/zac0/'}
+);
+});
 
 app.listen(port, () => console.log(`App server listening on port ${port}`));
